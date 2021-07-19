8ed7728c68660725fb6c1d1df3e8b43b1ace1426
Josh301 <nelsonjoshua301@gmail.com>
2021-07-19 12:35:44 +0300

    2021 july 19

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -67,11 +67,11 @@ $this->title = 'Web';
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6 d-flex flex-column justify-content-center">
-					<h2 data-aos="fade-up">WebVFD Solution</h2>
-					<h3 data-aos="fade-up" data-aos-delay="400">A new generation online
+					<h1 data-aos="fade-up">WebVFD Solution</h1>
+					<h2 data-aos="fade-up" data-aos-delay="400">A new generation online
 						cash register substitute of Electronic Fiscal Device (EFD)
 						designed for issurance of TRA Authorized fiscal receipts by
-						taxpayers in a real time.</h3>
+						taxpayers in a real time.</h2>
 					<div data-aos="fade-up" data-aos-delay="600">
 						<div class="text-center text-lg-start">
 							<a href="#pricing"
